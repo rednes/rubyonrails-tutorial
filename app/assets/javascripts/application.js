3978e1b226f4ff2e1d3cfcd59f71e187ea754ee3
rednes <528249+rednes@users.noreply.github.com>
2017-09-20 22:40:12 +0900

    Implement basic login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -8,6 +9,11 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
+
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # Add more helper methods to be used by all tests here...
 end
